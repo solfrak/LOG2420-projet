549f72f64629277a05f239bd89ff3620b48d4b0a
Samuel Laporte <laporte.samuel2000@gmail.com>
2021-01-22 13:08:56 -0500

    fin de premier cours

--- script.js
+++ script.js
@@ -4,13 +4,28 @@ fetch("./data.json")
     return response.json();
 })
 .then(data => {
-    test(data);
+    fTitre(data);
+    fBottin(data);
     
 });
 
 
-function test(data)
+function fTitre(data)
 {
-    document.getElementById("titre").innerHTML = data.titre;
-    document.getElementById("sous-titre").innerHTML = data.sousTitre;
+    document.getElementById("titre").innerHTML = data.donne[0].titre;
+    document.getElementById("sous-titre").innerHTML = data.donne[1].titre;
+    document.getElementById("fleche").innerHTML = data.donne[2].fleche;    
+}
+
+function fBottin(data)
+{
+    var text ="";
+    var i = 0
+    var x;
+    for (x in data.bottin){
+        text += data.bottin[x].name +" "+ data.bottin[x].lastName + data.bottin[x].poste + "<br>";
+        //document.write(text);
+    }
+    
+    document.getElementById("bottin").innerHTML = "<p>" + text + "</p>";
 }
