bc0176c91e014ea8e80dd2615cf1ef8488684304
julie-annej <julie-annej@users.noreply.github.com>
2021-02-05 12:01:01 -0500

    cours 2

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ fetch("./data.json")
 .then(data => {
     fTitre(data);
     fBottin(data);
-    
+    fProduit(data);
 });
 
 
@@ -20,12 +20,64 @@ function fTitre(data)
 function fBottin(data)
 {
     var text ="";
+    var email="";
     var i = 0
     var x;
-    for (x in data.bottin){
-        text += data.bottin[x].name +" "+ data.bottin[x].lastName + data.bottin[x].poste + "<br>";
-        //document.write(text);
-    }
+    // for (x in data.bottin){
+    //     text = data.bottin[x].name +" "+ data.bottin[x].lastName + " - "+ data.bottin[x].poste + "<br>";
+    //     email = data.bottin[x].email + "<br>";
+    //     document.getElementById("bottin_name").innerHTML += "<li class='nom'";
+    //     //document.write(text);
+        
+    //     document.getElementById("bottin_email").innerHTML +=  email;
+
+    // }
     
-    document.getElementById("bottin").innerHTML = "<p>" + text + "</p>";
+    
 }
+
+
+function buttonPlus(){
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+    
+}
+
+ function buttonMinus(){
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('number').value = value;
+ }
+
+ function popup(){
+    document.getElementById("popup").style.visibility = "visible";
+    console.log("clicked");
+ }
+ document.getElementById("firstarticle").addEventListener("click", popup);
+
+ 
+ //   function fProduit(data)
+ //  {
+ //      var text ="";
+ //      var i = 0
+    
+ //      var commande = document.getElementById("commandElemList");
+
+//      for (i in data.Produit){
+//          text = data.Produit[x].Produit;
+//          commande.innerHTML += "<div> <input type='text' class='commandElemInput' id='number' value='0'/>  <input type='button' onclick='buttonPlus()'  /><br><input type='button' onclick='buttonMinus()'  /><label id='commande' for='commandeName'>";
+//          commande.innerHTML += text + "</label><br></div> ";
+//          //document.write(text);
+for (i in data.Produit){
+    var original = document.getElementById('commande');
+    var clone = original.cloneNode(true); // clone
+    clone.id = "Produit"+=i;
+    document.getElementById(clone.id).value;
+    
+    original.appendChild(clone);
+
+}
+ //}
---
+++ script_login.js
@@ -0,0 +1,17 @@
+
+function login()
+{
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var benevoleCheckbox = document.getElementById("benevole");
+    console.log("failed to log in");
+    if(username == "jbradette" && password == "123456" && benevoleCheckbox.checked == true)
+    {   
+        location.href = './mainPage.html';
+    }
+    else{
+        document.getElementById("error").style.visibility = "visible";
+    }
+}
+
+document.getElementById("button_connecter").addEventListener("click", login);
