e26cba585b03da42991a8aa26c0f868f47568b91
solfrak <laporte.samuel2000@gmail.com>
2021-02-26 14:42:48 -0500

    fin du projet mais pas selon ayoub

--- script.js
+++ script.js
@@ -10,6 +10,33 @@ fetch("./data.json")
     fGuide(data);
 });
 
+function checkMediaWidth(){
+    if(window.innerWidth <=600){
+        document.getElementById("CommentaireBox").value = "Autres:";
+    }
+    else{
+        document.getElementById("CommentaireBox").value = "";
+    }
+}
+
+
+
+function fCommande(){
+    let savon = document.getElementById("produit1Input").value;
+    let guenille = document.getElementById("produit2Input").value;
+    let desodorisant = document.getElementById("produit3Input").value;
+    let ampoule = document.getElementById("produit4Input").value;
+    let commentaire = document.getElementById("CommentaireBox").value;
+    document.getElementById("Savon").innerHTML = "Savon: " + savon;
+    document.getElementById("Guenille").innerHTML ="Guenille: " + guenille;
+    document.getElementById("Désodorisant").innerHTML ="Désodorisant: " + desodorisant;
+    document.getElementById("Ampoules").innerHTML ="Ampoules: " + ampoule;
+    document.getElementById("Commentaire").innerHTML = commentaire;
+    console.log(document.getElementById("popup3"));
+    document.getElementById("CommandePopup").style.visibility = "visible";
+    document.getElementById("background").style.visibility ="visible";
+}
+
 function fGuide(data){
     let guide = document.getElementById("Guide");
     //Creation popup guide
@@ -18,22 +45,25 @@ function fGuide(data){
         let popup = document.createElement("div");
         popup.className = "popup2 popup";
         popup.id = "GuidePopup" + x;
-        popup.onclick = function(){
-            popupButton2(this.id);
-        };
+
         let myImg = document.createElement("img");
         myImg.className = "xButton";
         myImg.id = "xButton2"
         myImg.src = "https://image.flaticon.com/icons/png/512/458/458595.png";
 
-        let myTitle = document.createElement("span");
+        myImg.onclick = function(){
+            popupButton(popup.id);
+        };
+
+        let myTitle = document.createElement("h1");
         myTitle.className = "popup_title";
         myTitle.innerHTML = data.Guide[x].titre;
 
-        let myDescription = document.createElement("span");
+        let myDescription = document.createElement("p");
         myDescription.className = "popup_p popup2_p";
         myDescription.innerHTML = data.Guide[x].description;
 
+        popup.appendChild(myImg);
         popup.appendChild(myTitle);
         popup.appendChild(myDescription);
         popupContainer.appendChild(popup);
@@ -74,14 +104,15 @@ function fNouvelles(data){
     let popup = document.createElement("div");
     popup.className = "popup";
     popup.id = "popup" + x;
-    popup.onclick = function(){
-        popupButton(this.id);
-    }
     
     let myImg = document.createElement("img");
     myImg.className = "xButton";
     myImg.id = "xButton";
     myImg.src = "https://image.flaticon.com/icons/png/512/458/458595.png";
+
+    myImg.onclick = function(){
+        popupButton(popup.id);
+    }
     
     let myTitle = document.createElement("span");
     myTitle.className = "popup_title";
@@ -143,21 +174,22 @@ function fBottin(data)
     let text ="";
     let email="";
     let x;
+    let plus = "Plus..."
    
     for (x in data.bottin){
-        text = data.bottin[x].name +" "+ data.bottin[x].lastName + " - "+ data.bottin[x].poste + "<br>";
-        email = '<p>'+data.bottin[x].email+ '</p>' + "<img class='copy_icon' id ='copy_icon' src='/picture/u205.svg'>" + "<br>";
+        text = data.bottin[x].name +" "+ data.bottin[x].lastName + " - "+ data.bottin[x].poste;
+        email = '<p> <span>'+data.bottin[x].email +"</span>"+ "<img class='copy_icon' id ='copy_icon' src='/picture/u205.svg'>"+ '</p>';
         document.getElementById("bottin_liste").innerHTML += "<li><div class='nom'>" +  text +"</div>" + "<div class='email' onClick='copyToClipboard(this)'>" +  email + "</div>" + "</li>";
     }
+    document.getElementById("bottin_liste").innerHTML += "<div class='plus'>" + plus + "</div>";
 }
 
 
 
 function copyToClipboard(elem){
 
-    let division1 = elem.innerHTML.split('<p>'); //divise le string aux endroits avec <p>
-    let division2 = division1[1].split('</p>');  //divise le string aux endroits avec </p>
-    
+    let division1 = elem.innerHTML.split('<span>'); //divise le string aux endroits avec <p>
+    let division2 = division1[1].split('</span>');  //divise le string aux endroits avec </p>
     let text = document.createElement('textarea');
     text.value = division2[0]; // le email clicke
     document.body.appendChild(text);
@@ -181,19 +213,22 @@ function buttonPlus(id){
  function popup(elem){
 
     document.getElementById("popup" + elem).style.visibility = "visible";
+    document.getElementById("background").style.visibility = "visible";
+    
  }
  
  function popupButton(elemId){
     document.getElementById(elemId).style.visibility = "hidden";
+    document.getElementById("background").style.visibility = "hidden";
+
  }
 
  function popup2(elem){
     document.getElementById("GuidePopup" + elem).style.visibility = "visible";
+    document.getElementById("background").style.visibility = "visible";
+
  }
 
- function popupButton2(elem){
-    document.getElementById(elem).style.visibility = "hidden";
- }
  
  function popupExitButton(){
      document.getElementById("popupExit").style.visibility = "visible";
@@ -214,11 +249,9 @@ if(varPopupExitLoaded){
 
 }
 
- //document.getElementById("firstarticlecontainer").addEventListener("click", popup);
- document.getElementById("firstGuide").addEventListener("click", popup2);
- document.getElementById("xButton").addEventListener("click", popupButton);
- document.getElementById("xButton2").addEventListener("click", popupButton2);
- document.getElementById("u97_img").addEventListener("click", popupExitButton);
+
+ 
+document.getElementById("u97_img").addEventListener("click", popupExitButton);
 
 
 //Function pour les commandes
